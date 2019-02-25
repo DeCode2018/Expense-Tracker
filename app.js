8936f2aa84e9ef4f4d2abcc1e0b305deeccf6e8c
github username <github email address>
2019-02-25 17:26:22 -0500

    added img file for icon, added HomeVIewController, bootstrap and filters

--- app.js
+++ app.js
@@ -1,22 +1,16 @@
 var app = angular.module('expensesApp', []);
 
+app.controller('HomeViewController', ['$scope', function($scope){
+  $scope.appTitle = '  |   Simple Expense Tracker';
+}]);
+
 app.controller('ExpensesViewController', ['$scope', function($scope){
-  $scope.items = [
-    {
-      name:'pizza',
-      ingredients: ['cheese', 'pepperoni', 'sausage','mushroom']
-    },
-    {
-      name:'sushi',
-      ingredients: ['shrimp', 'spicy mayo', 'seaweed','rice']
-    },
-    {
-      name:'poke',
-      ingredients: ['shrimp', 'spinach', 'pineapple','cilantro']
-    }
-
-  ];
-
-
-
+  $scope.expenses = [
+    {description:'food', amount:10, date: '2018-10-01'},
+    {description:'tickets', amount:11, date: '2018-10-02'},
+    {description:'food', amount:12, date: '2018-10-03'},
+    {description:'phone credit', amount:13, date: '2018-10-04'},
+    {description:'bills', amount:14, date: '2018-10-05'},
+    {description:'food', amount:15, date: '2018-10-06'},
+  ]
 }]);
