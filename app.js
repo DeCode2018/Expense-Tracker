72f0f7be6bc3b5b30068a73395762e9567ff0c22
github username <github email address>
2019-02-25 12:47:08 -0500

    tested out two way data binding

--- app.js
+++ app.js
@@ -1,6 +1,18 @@
 var app = angular.module('expensesApp', []);
 
 app.controller('ExpensesViewController', ['$scope', function($scope){
-  $scope.name = 'The Developer';
+  $scope.expense = {
+    description: 'food',
+    amount: 10
+
+  };
+
+  $scope.phrase = 'the sky is blue';
+
+  $scope.increaseButton = function(){
+    $scope.expense.amount++;
+  }
+
+
 
 }]);
